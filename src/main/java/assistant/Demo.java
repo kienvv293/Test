358f464d2b1f0db422050aa550c692801d1f4db7
Vu Van Kien <vuvankien@luvina.net>
2019-07-30 16:21:03 +0700

    111:Ticket #1111111111 Redmine #9034516 ----------Test plugin sonarrrrrr

--- src/main/java/assistant/Demo.java
+++ src/main/java/assistant/Demo.java
@@ -3,19 +3,7 @@ package assistant;
 public class Demo {
 	int a;
 
-	private void test() {
-
-	}
-
-	private void test1() {
-
-	}
-	
-	private void test2() {
-
-	}
-	
-	private void test3() {
+	private void demo() {
 
 	}
 }
