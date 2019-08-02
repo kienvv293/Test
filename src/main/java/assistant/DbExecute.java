da567b8f6d65ae5bc337d46aef88294004a90cf7
Vu Van Kien <vuvankien@luvina.net>
2019-08-02 15:50:08 +0700

    12333:Ticket #1234566 Redmine #9034516 ----------Test plugin sonar githubbbbb

--- src/main/java/assistant/DbExecute.java
+++ src/main/java/assistant/DbExecute.java
@@ -8,15 +8,8 @@ package assistant;
  *
  */
 public class DbExecute {
-
-	int a;
-	int b;
-
-	private void test() {
-
-	}
-
-	private void test1() {
-
+	private int demo() {
+		int a;
+		return 0;
 	}
 }
